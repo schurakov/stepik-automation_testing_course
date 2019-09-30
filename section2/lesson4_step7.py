a62458a9190dd8780e176b4827b11dd936ca99d5
Stepan Churakov <churakov.s@gmail.com>
2019-09-30 23:49:40 +0300

    add: expected conditions usage upd: delete excess import

--- section2/lesson4_step7.py
+++ section2/lesson4_step7.py
@@ -1,8 +1,11 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
 import math
 
-link = "http://suninjuly.github.io/redirect_accept.html"
+link = "http://suninjuly.github.io/explicit_wait2.html"
 
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
@@ -11,11 +14,11 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    button = browser.find_element_by_css_selector("button[type='submit']")
-    button.click()
 
-    new_window = browser.window_handles[1]
-    browser.switch_to.window(new_window)
+
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    button_book = browser.find_element_by_css_selector("button#book")
+    button_book.click()
     
     #паршу значение
     x_element = browser.find_element_by_id("input_value")
