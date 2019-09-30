e9de9b93e264fd90f3595f56f5002d38b4c49784
Stepan Churakov <churakov.s@gmail.com>
2019-09-30 22:57:45 +0300

    add: alert confirm

---
+++ section2/lesson3_step3.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import os
+import math
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button[type='submit']")
+    button.click()
+
+    alert = browser.switch_to.alert
+    alert.accept()
+    
+    #паршу значение
+    x_element = browser.find_element_by_id("input_value")
+    x = calc(int(x_element.text))
+
+    #ввожу в инпут вычисленный ответ
+    input1 = browser.find_element_by_css_selector("input#answer")
+    input1.send_keys(x)
+
+    button = browser.find_element_by_css_selector("button[type='submit']")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
