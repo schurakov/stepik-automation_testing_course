bdb0b2563d33569494cd3d784e3eba6b02a31e06
Stepan Churakov <churakov.s@gmail.com>
2020-08-15 19:12:21 +0300

    fix css-selectors in lesson6_step9.py

--- section1/lesson6_step9.py
+++ section1/lesson6_step9.py
@@ -7,11 +7,11 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_css_selector(".first")
+    input1 = browser.find_element_by_css_selector(".first_block .first")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_class_name("second")
+    input2 = browser.find_element_by_css_selector(".first_block .second")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("third")
+    input3 = browser.find_element_by_css_selector(".first_block .third")
     input3.send_keys("ipetrov@test.test")
     
     # Отправляем заполненную форму
